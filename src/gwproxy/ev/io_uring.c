realyukii/gwproxy

commit 06fa2ea2ba4acee27d03b7f467012b403df1f48c

io_uring: Replace `prep_shutdown()` with `prep_cancel()`
Replace `prep_shutdown()` with `prep_cancel()` to avoid io-wq workers
spawn. `prep_shutdown()` is always punted to io-wq because internally
it sets `REQ_F_FORCE_ASYNC`.

Signed-off-by: Ammar Faizi <ammarfaizi2@gnuweeb.org>

src/gwproxy/ev/io_uring.c
@@ -319,20 +319,20 @@ static void shutdown_gcp(struct gwp_wrk *w, struct gwp_conn_pair *gcp)
 		return;
 
 	if (gcp->target.fd >= 0) {
-		pr_dbg(&ctx->lh, "Shutting down target connection (fd=%d)", gcp->target.fd);
+		pr_dbg(&ctx->lh, "Cancelling target recv (fd=%d)", gcp->target.fd);
 		s = get_sqe_nofail(w);
-		io_uring_prep_shutdown(s, gcp->target.fd, SHUT_RDWR);
+		io_uring_prep_cancel_fd(s, gcp->target.fd, 0);
 		io_uring_sqe_set_data(s, gcp);
-		s->user_data |= EV_BIT_TARGET_SHUTDOWN;
+		s->user_data |= EV_BIT_TARGET_CANCEL;
 		get_gcp(gcp);
 	}
 
 	if (gcp->client.fd >= 0) {
-		pr_dbg(&ctx->lh, "Shutting down client connection (fd=%d)", gcp->client.fd);
+		pr_dbg(&ctx->lh, "Cancelling client recv (fd=%d)", gcp->client.fd);
 		s = get_sqe_nofail(w);
-		io_uring_prep_shutdown(s, gcp->client.fd, SHUT_RDWR);
+		io_uring_prep_cancel_fd(s, gcp->client.fd, 0);
 		io_uring_sqe_set_data(s, gcp);
-		s->user_data |= EV_BIT_CLIENT_SHUTDOWN;
+		s->user_data |= EV_BIT_CLIENT_CANCEL;
 		get_gcp(gcp);
 	}
 
@@ -635,14 +635,14 @@ static int handle_event(struct gwp_wrk *w, struct io_uring_cqe *cqe)
 		pr_dbg(&ctx->lh, "Handling target send event: %d", cqe->res);
 		r = handle_ev_target_send(w, udata, cqe);
 		break;
-	case EV_BIT_CLIENT_SHUTDOWN:
-		pr_dbg(&ctx->lh, "Handling client shutdown event: %d", cqe->res);
+	case EV_BIT_TARGET_CANCEL:
+		pr_dbg(&ctx->lh, "Handling target cancel event: %d", cqe->res);
 		assert(gcp->flags & GWP_CONN_FLAG_IS_SHUTDOWN);
 		r = 0;
 		break;
-	case EV_BIT_TARGET_SHUTDOWN:
-		pr_dbg(&ctx->lh, "Handling target shutdown event: %d", cqe->res);
-		assert(gcp->flags & GWP_CONN_FLAG_IS_SHUTDOWN);
+	case EV_BIT_CLIENT_CANCEL:
+		pr_dbg(&ctx->lh, "Handling client cancel event: %d", cqe->res);
+		assert(gcp->flags & GWP_CONN_FLAG_IS_CANCEL);
 		r = 0;
 		break;
 	case EV_BIT_MSG_RING:

src/gwproxy/gwproxy.h
@@ -57,8 +57,8 @@ enum {
 
 	EV_BIT_CLOSE		= (9ull << 48ull),
 	EV_BIT_TARGET_CONNECT	= (10ull << 48ull),
-	EV_BIT_TARGET_SHUTDOWN	= (11ull << 48ull),
-	EV_BIT_CLIENT_SHUTDOWN	= (12ull << 48ull),
+	EV_BIT_TARGET_CANCEL	= (11ull << 48ull),
+	EV_BIT_CLIENT_CANCEL	= (12ull << 48ull),
 	EV_BIT_TIMER_DEL	= (13ull << 48ull),
 	EV_BIT_TARGET_SEND	= (14ull << 48ull),
 	EV_BIT_CLIENT_SEND	= (15ull << 48ull),

commit be5eeab266d36129f722fb023a7d3715a5e125cb

io_uring: Rename GWP_CONN_FLAG_IS_SHUTDOWN to GWP_CONN_FLAG_IS_CANCEL
A previous commit forgot to replace GWP_CONN_FLAG_IS_SHUTDOWN and leading to
build error on debug mode, it wasn't detected on release mode.

Fixes: 06fa2ea2ba4a ("io_uring: Replace prep_shutdown() with prep_cancel()")
Signed-off-by: Ahmad Gani <reyuki@gnuweeb.org>
Link: https://lore.gnuweeb.org/gwml/20250729034719.190563-4-reyuki@gnuweeb.org
Signed-off-by: Ammar Faizi <ammarfaizi2@gnuweeb.org>

src/gwproxy/ev/io_uring.c
@@ -318,7 +318,7 @@ static void shutdown_gcp(struct gwp_wrk *w, struct gwp_conn_pair *gcp)
 	struct gwp_ctx *ctx = w->ctx;
 	struct io_uring_sqe *s;
 
-	if (gcp->flags & GWP_CONN_FLAG_IS_SHUTDOWN)
+	if (gcp->flags & GWP_CONN_FLAG_IS_CANCEL)
 		return;
 
 	if (gcp->target.fd >= 0) {
@@ -339,7 +339,7 @@ static void shutdown_gcp(struct gwp_wrk *w, struct gwp_conn_pair *gcp)
 		get_gcp(gcp);
 	}
 
-	gcp->flags |= GWP_CONN_FLAG_IS_SHUTDOWN;
+	gcp->flags |= GWP_CONN_FLAG_IS_CANCEL;
 }
 
 static int arm_gcp(struct gwp_wrk *w, struct gwp_conn_pair *gcp)
@@ -640,7 +640,7 @@ static int handle_event(struct gwp_wrk *w, struct io_uring_cqe *cqe)
 		break;
 	case EV_BIT_TARGET_CANCEL:
 		pr_dbg(&ctx->lh, "Handling target cancel event: %d", cqe->res);
-		assert(gcp->flags & GWP_CONN_FLAG_IS_SHUTDOWN);
+		assert(gcp->flags & GWP_CONN_FLAG_IS_CANCEL);
 		r = 0;
 		break;
 	case EV_BIT_CLIENT_CANCEL:
@@ -661,7 +661,7 @@ static int handle_event(struct gwp_wrk *w, struct io_uring_cqe *cqe)
 	gcp = udata;
 
 	if ((gcp->flags & GWP_CONN_FLAG_IS_DYING) &&
-	    !(gcp->flags & GWP_CONN_FLAG_IS_SHUTDOWN))
+	    !(gcp->flags & GWP_CONN_FLAG_IS_CANCEL))
 		shutdown_gcp(w, gcp);
 
 	put_gcp(w, gcp);

src/gwproxy/gwproxy.h
@@ -99,7 +99,7 @@ enum {
 	 */
 	GWP_CONN_FLAG_NO_CLOSE_FD	= (1ull << 0ull),
 	GWP_CONN_FLAG_IS_DYING		= (1ull << 1ull),
-	GWP_CONN_FLAG_IS_SHUTDOWN	= (1ull << 2ull),
+	GWP_CONN_FLAG_IS_CANCEL		= (1ull << 2ull),
 };
 
 struct gwp_conn_pair {