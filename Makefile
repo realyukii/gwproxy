realyukii/gwproxy

commit 10ee5b83b50f9f454b893e36e8fc5611615c1ffd

io_uring: Add liburing to the code base
Make the build process easier.
Some people may not have had liburing installed on their system.

Signed-off-by: Ahmad Gani <reyuki@gnuweeb.org>
Link: https://lore.gnuweeb.org/gwml/20250729034719.190563-2-reyuki@gnuweeb.org
Signed-off-by: Ammar Faizi <ammarfaizi2@gnuweeb.org>

Makefile
@@ -9,6 +9,7 @@ LIBS = -lpthread
 DEPFLAGS = -MMD -MP -MF $@.d
 LDFLAGS_SHARED = $(LDFLAGS) -shared -fpic -fPIC
 GWPROXY_DIR = ./src/gwproxy
+LIBURING_DIR = ./src/liburing/src
 
 ifeq ($(SANITIZE),1)
 	CFLAGS += -fsanitize=address -fsanitize=undefined
@@ -30,6 +31,8 @@ ifeq ($(STATIC),1)
 	LDFLAGS += -static
 endif
 
+LIBURING_TARGET = $(LIBURING_DIR)/liburing.a
+
 GWPROXY_TARGET = gwproxy
 GWPROXY_CC_SOURCES = \
 	$(GWPROXY_DIR)/gwproxy.c \
@@ -39,7 +42,7 @@ GWPROXY_CC_SOURCES = \
 
 ifeq ($(CONFIG_IO_URING),1)
 	CFLAGS += -DCONFIG_IO_URING
-	LIBS += /usr/lib/liburing.a
+	LIBS += $(LIBURING_TARGET)
 	GWPROXY_CC_SOURCES += $(GWPROXY_DIR)/ev/io_uring.c
 endif
 
@@ -66,7 +69,10 @@ ALL_DEPFILES = $(ALL_OBJECTS:.o=.o.d)
 
 ALL_GWPROXY_OBJECTS = $(GWPROXY_OBJECTS) $(LIBGWPSOCKS5_OBJECTS) $(LIBGWDNS_OBJECTS)
 
-all: $(GWPROXY_TARGET) $(LIBGWPSOCKS5_TARGET) $(LIBGWDNS_TARGET)
+all: $(GWPROXY_TARGET) $(LIBGWPSOCKS5_TARGET) $(LIBGWDNS_TARGET) $(LIBURING_TARGET)
+
+$(LIBURING_TARGET): $(LIBURING_DIR)
+	@$(MAKE) -C $^
 
 $(GWPROXY_TARGET): $(ALL_GWPROXY_OBJECTS)
 	$(CC) $(LDFLAGS) -o $@ $^ $(LIBS)

.gitmodules
@@ -1,0 +1,4 @@
+[submodule "src/liburing"]
+	path = src/liburing
+	url = https://github.com/axboe/liburing
+	branch = master

commit e7ad9206c18ef2f3360a26989bc826f08c192ccf

Makefile: Don't append LIBURING_TARGET to LIBS
Don't append LIBURING_TARGET to LIBS because LIBS is used for all
libraries while LIBURING_TARGET is only used for LIBURING_TARGET.
While in there, also ensure LIBURING_DIR is properly initialized.

Fixes: 10ee5b83b50f ("io_uring: Add liburing to the code base")
Signed-off-by: Ammar Faizi <ammarfaizi2@gnuweeb.org>

Makefile
@@ -42,7 +42,6 @@ GWPROXY_CC_SOURCES = \
 
 ifeq ($(CONFIG_IO_URING),1)
 	CFLAGS += -DCONFIG_IO_URING
-	LIBS += $(LIBURING_TARGET)
 	GWPROXY_CC_SOURCES += $(GWPROXY_DIR)/ev/io_uring.c
 endif
 
@@ -69,12 +68,15 @@ ALL_DEPFILES = $(ALL_OBJECTS:.o=.o.d)
 
 ALL_GWPROXY_OBJECTS = $(GWPROXY_OBJECTS) $(LIBGWPSOCKS5_OBJECTS) $(LIBGWDNS_OBJECTS)
 
-all: $(GWPROXY_TARGET) $(LIBGWPSOCKS5_TARGET) $(LIBGWDNS_TARGET) $(LIBURING_TARGET)
+all: $(GWPROXY_TARGET) $(LIBGWPSOCKS5_TARGET) $(LIBGWDNS_TARGET)
 
-$(LIBURING_TARGET): $(LIBURING_DIR)
-	@$(MAKE) -C $^
+$(LIBURING_DIR)/Makefile:
+	git submodule update --init --recursive;
 
-$(GWPROXY_TARGET): $(ALL_GWPROXY_OBJECTS)
+$(LIBURING_TARGET): $(LIBURING_DIR)/Makefile
+	@$(MAKE) -C $(LIBURING_DIR) library
+
+$(GWPROXY_TARGET): $(ALL_GWPROXY_OBJECTS) $(LIBURING_TARGET)
 	$(CC) $(LDFLAGS) -o $@ $^ $(LIBS)
 
 $(LIBGWPSOCKS5_TARGET): $(LIBGWPSOCKS5_OBJECTS)