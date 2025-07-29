realyukii/gwproxy

commit 10ee5b83b50f9f454b893e36e8fc5611615c1ffd

io_uring: Add liburing to the code base
Make the build process easier.
Some people may not have had liburing installed on their system.

Signed-off-by: Ahmad Gani <reyuki@gnuweeb.org>
Link: https://lore.gnuweeb.org/gwml/20250729034719.190563-2-reyuki@gnuweeb.org
Signed-off-by: Ammar Faizi <ammarfaizi2@gnuweeb.org>

Makefile
@@ -9,6 +9,7 @@ LIBS = -lpthread
 DEPFLAGS = -MMD -MP -MF $@.d
 LDFLAGS_SHARED = $(LDFLAGS) -shared -fpic -fPIC
 GWPROXY_DIR = ./src/gwproxy
+LIBURING_DIR = ./src/liburing/src
 
 ifeq ($(SANITIZE),1)
 	CFLAGS += -fsanitize=address -fsanitize=undefined
@@ -30,6 +31,8 @@ ifeq ($(STATIC),1)
 	LDFLAGS += -static
 endif
 
+LIBURING_TARGET = $(LIBURING_DIR)/liburing.a
+
 GWPROXY_TARGET = gwproxy
 GWPROXY_CC_SOURCES = \
 	$(GWPROXY_DIR)/gwproxy.c \
@@ -39,7 +42,7 @@ GWPROXY_CC_SOURCES = \
 
 ifeq ($(CONFIG_IO_URING),1)
 	CFLAGS += -DCONFIG_IO_URING
-	LIBS += /usr/lib/liburing.a
+	LIBS += $(LIBURING_TARGET)
 	GWPROXY_CC_SOURCES += $(GWPROXY_DIR)/ev/io_uring.c
 endif
 
@@ -66,7 +69,10 @@ ALL_DEPFILES = $(ALL_OBJECTS:.o=.o.d)
 
 ALL_GWPROXY_OBJECTS = $(GWPROXY_OBJECTS) $(LIBGWPSOCKS5_OBJECTS) $(LIBGWDNS_OBJECTS)
 
-all: $(GWPROXY_TARGET) $(LIBGWPSOCKS5_TARGET) $(LIBGWDNS_TARGET)
+all: $(GWPROXY_TARGET) $(LIBGWPSOCKS5_TARGET) $(LIBGWDNS_TARGET) $(LIBURING_TARGET)
+
+$(LIBURING_TARGET): $(LIBURING_DIR)
+	@$(MAKE) -C $^
 
 $(GWPROXY_TARGET): $(ALL_GWPROXY_OBJECTS)
 	$(CC) $(LDFLAGS) -o $@ $^ $(LIBS)

.gitmodules
@@ -1,0 +1,4 @@
+[submodule "src/liburing"]
+	path = src/liburing
+	url = https://github.com/axboe/liburing
+	branch = master

commit 76045a47aeaf1ef482d2afa0b2e9c54ae4c8847a

Makefile: Fix liburing build error
To build liburing, initially, the ./configure script needs to be
invoked. The default 'make' target when invoked in the liburing/
directory will invoke the ./configure script if it's needed.

Don't run the 'make' command in the liburing/src/ directory. Run it in
liburing/ directory to cover the ./configure script invokation.

Running the make command in liburing/src/ only works when the ./configure
script has been invoked first. If it has not been invoked, the build will
lead to this error:

  $ make -j
  make[1]: Entering directory '/home/ammarfaizi2/p/gwproxy/src/liburing/src'
  Makefile:37: ../config-host.mak: No such file or directory
  make[1]: *** No rule to make target '../config-host.mak'.  Stop.
  make[1]: Leaving directory '/home/ammarfaizi2/p/gwproxy/src/liburing/src'
  make: *** [Makefile:75: src/liburing/src/liburing.a] Error 2

Fixes: 10ee5b83b50f ("io_uring: Add liburing to the code base")
Signed-off-by: Ammar Faizi <ammarfaizi2@gnuweeb.org>

Makefile
@@ -9,7 +9,7 @@ LIBS = -lpthread
 DEPFLAGS = -MMD -MP -MF $@.d
 LDFLAGS_SHARED = $(LDFLAGS) -shared -fpic -fPIC
 GWPROXY_DIR = ./src/gwproxy
-LIBURING_DIR = ./src/liburing/src
+LIBURING_DIR = ./src/liburing
 
 ifeq ($(SANITIZE),1)
 	CFLAGS += -fsanitize=address -fsanitize=undefined
@@ -31,7 +31,7 @@ ifeq ($(STATIC),1)
 	LDFLAGS += -static
 endif
 
-LIBURING_TARGET = $(LIBURING_DIR)/liburing.a
+LIBURING_TARGET = $(LIBURING_DIR)/src/liburing.a
 
 GWPROXY_TARGET = gwproxy
 GWPROXY_CC_SOURCES = \