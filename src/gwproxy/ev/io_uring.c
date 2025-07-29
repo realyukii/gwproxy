realyukii/gwproxy

commit 06fa2ea2ba4acee27d03b7f467012b403df1f48c

io_uring: Replace `prep_shutdown()` with `prep_cancel()`
Replace `prep_shutdown()` with `prep_cancel()` to avoid io-wq workers
spawn. `prep_shutdown()` is always punted to io-wq because internally
it sets `REQ_F_FORCE_ASYNC`.

Signed-off-by: Ammar Faizi <ammarfaizi2@gnuweeb.org>

src/gwproxy/ev/io_uring.c
@@ -319,20 +319,20 @@ static void shutdown_gcp(struct gwp_wrk *w, struct gwp_conn_pair *gcp)
 		return;
 
 	if (gcp->target.fd >= 0) {
-		pr_dbg(&ctx->lh, "Shutting down target connection (fd=%d)", gcp->target.fd);
+		pr_dbg(&ctx->lh, "Cancelling target recv (fd=%d)", gcp->target.fd);
 		s = get_sqe_nofail(w);
-		io_uring_prep_shutdown(s, gcp->target.fd, SHUT_RDWR);
+		io_uring_prep_cancel_fd(s, gcp->target.fd, 0);
 		io_uring_sqe_set_data(s, gcp);
-		s->user_data |= EV_BIT_TARGET_SHUTDOWN;
+		s->user_data |= EV_BIT_TARGET_CANCEL;
 		get_gcp(gcp);
 	}
 
 	if (gcp->client.fd >= 0) {
-		pr_dbg(&ctx->lh, "Shutting down client connection (fd=%d)", gcp->client.fd);
+		pr_dbg(&ctx->lh, "Cancelling client recv (fd=%d)", gcp->client.fd);
 		s = get_sqe_nofail(w);
-		io_uring_prep_shutdown(s, gcp->client.fd, SHUT_RDWR);
+		io_uring_prep_cancel_fd(s, gcp->client.fd, 0);
 		io_uring_sqe_set_data(s, gcp);
-		s->user_data |= EV_BIT_CLIENT_SHUTDOWN;
+		s->user_data |= EV_BIT_CLIENT_CANCEL;
 		get_gcp(gcp);
 	}
 
@@ -635,14 +635,14 @@ static int handle_event(struct gwp_wrk *w, struct io_uring_cqe *cqe)
 		pr_dbg(&ctx->lh, "Handling target send event: %d", cqe->res);
 		r = handle_ev_target_send(w, udata, cqe);
 		break;
-	case EV_BIT_CLIENT_SHUTDOWN:
-		pr_dbg(&ctx->lh, "Handling client shutdown event: %d", cqe->res);
+	case EV_BIT_TARGET_CANCEL:
+		pr_dbg(&ctx->lh, "Handling target cancel event: %d", cqe->res);
 		assert(gcp->flags & GWP_CONN_FLAG_IS_SHUTDOWN);
 		r = 0;
 		break;
-	case EV_BIT_TARGET_SHUTDOWN:
-		pr_dbg(&ctx->lh, "Handling target shutdown event: %d", cqe->res);
-		assert(gcp->flags & GWP_CONN_FLAG_IS_SHUTDOWN);
+	case EV_BIT_CLIENT_CANCEL:
+		pr_dbg(&ctx->lh, "Handling client cancel event: %d", cqe->res);
+		assert(gcp->flags & GWP_CONN_FLAG_IS_CANCEL);
 		r = 0;
 		break;
 	case EV_BIT_MSG_RING:

src/gwproxy/gwproxy.h
@@ -57,8 +57,8 @@ enum {
 
 	EV_BIT_CLOSE		= (9ull << 48ull),
 	EV_BIT_TARGET_CONNECT	= (10ull << 48ull),
-	EV_BIT_TARGET_SHUTDOWN	= (11ull << 48ull),
-	EV_BIT_CLIENT_SHUTDOWN	= (12ull << 48ull),
+	EV_BIT_TARGET_CANCEL	= (11ull << 48ull),
+	EV_BIT_CLIENT_CANCEL	= (12ull << 48ull),
 	EV_BIT_TIMER_DEL	= (13ull << 48ull),
 	EV_BIT_TARGET_SEND	= (14ull << 48ull),
 	EV_BIT_CLIENT_SEND	= (15ull << 48ull),

commit 1bdc3e557b14fdfc5c7191a68e4ccc9e09286f9d

io_uring: Fix uninitialized read when debug mode is on
GCC correctly warns uinitialized read:

  In file included from src/gwproxy/ev/io_uring.c:19:
  src/gwproxy/ev/io_uring.c: In function ‘handle_event’:
  src/gwproxy/ev/io_uring.c:643:27: warning: ‘gcp’ may be used uninitialized [-Wmaybe-uninitialized]
    643 |                 assert(gcp->flags & GWP_CONN_FLAG_IS_CANCEL);
        |                        ~~~^~~~~~~
  src/gwproxy/ev/io_uring.c:605:31: note: ‘gcp’ was declared here
    605 |         struct gwp_conn_pair *gcp;
        |                               ^~~

Fixes: 06fa2ea2ba4a ("io_uring: Replace `prep_shutdown()` with `prep_cancel()`")
Signed-off-by: Ammar Faizi <ammarfaizi2@gnuweeb.org>

src/gwproxy/ev/io_uring.c
@@ -639,11 +639,13 @@ static int handle_event(struct gwp_wrk *w, struct io_uring_cqe *cqe)
 		r = handle_ev_target_send(w, udata, cqe);
 		break;
 	case EV_BIT_TARGET_CANCEL:
+		gcp = udata;
 		pr_dbg(&ctx->lh, "Handling target cancel event: %d", cqe->res);
 		assert(gcp->flags & GWP_CONN_FLAG_IS_CANCEL);
 		r = 0;
 		break;
 	case EV_BIT_CLIENT_CANCEL:
+		gcp = udata;
 		pr_dbg(&ctx->lh, "Handling client cancel event: %d", cqe->res);
 		assert(gcp->flags & GWP_CONN_FLAG_IS_CANCEL);
 		r = 0;