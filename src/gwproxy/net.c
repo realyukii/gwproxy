realyukii/gwproxy

commit 70b88aa9a05411f52418a71240ad4a050e91bcd0

dnslookup: Split common functionality and struct into net.h and net.c
List of extracted stuff:
- convert_str_to_ssaddr function to net.c.
- convert_ssaddr_to_str function to net.c.
- struct gwp_sockaddr to net.h
- FULL_ADDRSTRLEN constant to net.h

It's better to split these common function that used in more
than one place.

Signed-off-by: Ahmad Gani <reyuki@gnuweeb.org>
Reviewed-by: Alviro Iskandar Setiawan <alviro.iskandar@gnuweeb.org>
Link: https://lore.gnuweeb.org/gwml/20250806035727.216702-2-reyuki@gnuweeb.org
Signed-off-by: Ammar Faizi <ammarfaizi2@gnuweeb.org>

src/gwproxy/net.c
@@ -1,0 +1,99 @@
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <stddef.h>
+#include <errno.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <netdb.h>
+#include <gwproxy/net.h>
+#include <gwproxy/common.h>
+
+__cold
+int convert_str_to_ssaddr(const char *str, struct gwp_sockaddr *gs)
+{
+	static const struct addrinfo hints = {
+		.ai_family = AF_UNSPEC,
+		.ai_socktype = SOCK_STREAM,
+	};
+	char host[NI_MAXHOST], port[6], *p;
+	struct addrinfo *res, *ai;
+	bool found = false;
+	size_t l;
+	int r;
+
+	if (*str == '[') {
+		p = strchr(++str, ']');
+		if (!p)
+			return -EINVAL;
+		l = p - str;
+		if (l >= sizeof(host))
+			return -EINVAL;
+		p++;
+		if (*p != ':')
+			return -EINVAL;
+	} else {
+		p = strchr(str, ':');
+		if (!p)
+			return -EINVAL;
+		l = p - str;
+		if (l >= sizeof(host))
+			return -EINVAL;
+	}
+
+	strncpy(host, str, l);
+	host[l] = '\0';
+	strncpy(port, p + 1, sizeof(port) - 1);
+	port[sizeof(port) - 1] = '\0';
+
+	r = getaddrinfo(host, port, &hints, &res);
+	if (r)
+		return -EINVAL;
+	if (!res)
+		return -EINVAL;
+
+	memset(gs, 0, sizeof(*gs));
+	for (ai = res; ai; ai = ai->ai_next) {
+		if (ai->ai_family == AF_INET) {
+			gs->i4 = *(struct sockaddr_in *)ai->ai_addr;
+			found = true;
+			break;
+		} else if (ai->ai_family == AF_INET6) {
+			gs->i6 = *(struct sockaddr_in6 *)ai->ai_addr;
+			found = true;
+			break;
+		}
+	}
+
+	freeaddrinfo(res);
+	return found ? 0 : -EINVAL;
+}
+
+__cold
+int convert_ssaddr_to_str(char buf[FULL_ADDRSTRLEN],
+			const struct gwp_sockaddr *gs)
+{
+	int f = gs->sa.sa_family;
+	uint16_t port = 0;
+	size_t l;
+
+	if (f == AF_INET) {
+		if (!inet_ntop(f, &gs->i4.sin_addr, buf, INET_ADDRSTRLEN))
+			return -EINVAL;
+		l = strlen(buf);
+		port = ntohs(gs->i4.sin_port);
+	} else if (f == AF_INET6) {
+		buf[0] = '[';
+		if (!inet_ntop(f, &gs->i6.sin6_addr, buf + 1, INET6_ADDRSTRLEN))
+			return -EINVAL;
+		l = strlen(buf);
+		buf[l++] = ']';
+		port = ntohs(gs->i6.sin6_port);
+	} else {
+		return -EINVAL;
+	}
+
+	buf[l++] = ':';
+	snprintf(buf + l, FULL_ADDRSTRLEN - l, "%hu", port);
+	return 0;
+}

Makefile
@@ -29,6 +29,7 @@ GWPROXY_TARGET = gwproxy
 GWPROXY_CC_SOURCES = \
 	$(GWPROXY_DIR)/gwproxy.c \
 	$(GWPROXY_DIR)/log.c \
+	$(GWPROXY_DIR)/net.c \
 	$(GWPROXY_DIR)/ev/epoll.c
 
 GWPROXY_OBJECTS = $(GWPROXY_CC_SOURCES:%.c=%.c.o)

src/gwproxy/dns.h
@@ -9,16 +9,9 @@
 #include <stdatomic.h>
 #include <stdbool.h>
 #include <netinet/in.h>
+#include <gwproxy/net.h>
 
 struct gwp_dns_wrk;
-
-struct gwp_sockaddr {
-	union {
-		struct sockaddr		sa;
-		struct sockaddr_in	i4;
-		struct sockaddr_in6	i6;
-	};
-};
 
 struct gwp_dns_entry {
 	char			*name;

src/gwproxy/gwproxy.c
@@ -259,35 +259,6 @@ einval:
 #define FULL_ADDRSTRLEN (INET6_ADDRSTRLEN + sizeof(":65535[]") - 1)
 
 __hot
-static int convert_ssaddr_to_str(char buf[FULL_ADDRSTRLEN],
-				 const struct gwp_sockaddr *gs)
-{
-	int f = gs->sa.sa_family;
-	uint16_t port = 0;
-	size_t l;
-
-	if (f == AF_INET) {
-		if (!inet_ntop(f, &gs->i4.sin_addr, buf, INET_ADDRSTRLEN))
-			return -EINVAL;
-		l = strlen(buf);
-		port = ntohs(gs->i4.sin_port);
-	} else if (f == AF_INET6) {
-		buf[0] = '[';
-		if (!inet_ntop(f, &gs->i6.sin6_addr, buf + 1, INET6_ADDRSTRLEN))
-			return -EINVAL;
-		l = strlen(buf);
-		buf[l++] = ']';
-		port = ntohs(gs->i6.sin6_port);
-	} else {
-		return -EINVAL;
-	}
-
-	buf[l++] = ':';
-	snprintf(buf + l, FULL_ADDRSTRLEN - l, "%hu", port);
-	return 0;
-}
-
-__hot
 const char *ip_to_str(const struct gwp_sockaddr *gs)
 {
 	static __thread char buf[8][FULL_ADDRSTRLEN];
@@ -295,66 +266,6 @@ const char *ip_to_str(const struct gwp_sockaddr *gs)
 	char *bp = buf[idx++ % 8];
 
 	return convert_ssaddr_to_str(bp, gs) ? NULL : bp;
-}
-
-__cold
-static int convert_str_to_ssaddr(const char *str, struct gwp_sockaddr *gs)
-{
-	static const struct addrinfo hints = {
-		.ai_family = AF_UNSPEC,
-		.ai_socktype = SOCK_STREAM,
-	};
-	char host[NI_MAXHOST], port[6], *p;
-	struct addrinfo *res, *ai;
-	bool found = false;
-	size_t l;
-	int r;
-
-	if (*str == '[') {
-		p = strchr(++str, ']');
-		if (!p)
-			return -EINVAL;
-		l = p - str;
-		if (l >= sizeof(host))
-			return -EINVAL;
-		p++;
-		if (*p != ':')
-			return -EINVAL;
-	} else {
-		p = strchr(str, ':');
-		if (!p)
-			return -EINVAL;
-		l = p - str;
-		if (l >= sizeof(host))
-			return -EINVAL;
-	}
-
-	strncpy(host, str, l);
-	host[l] = '\0';
-	strncpy(port, p + 1, sizeof(port) - 1);
-	port[sizeof(port) - 1] = '\0';
-
-	r = getaddrinfo(host, port, &hints, &res);
-	if (r)
-		return -EINVAL;
-	if (!res)
-		return -EINVAL;
-
-	memset(gs, 0, sizeof(*gs));
-	for (ai = res; ai; ai = ai->ai_next) {
-		if (ai->ai_family == AF_INET) {
-			gs->i4 = *(struct sockaddr_in *)ai->ai_addr;
-			found = true;
-			break;
-		} else if (ai->ai_family == AF_INET6) {
-			gs->i6 = *(struct sockaddr_in6 *)ai->ai_addr;
-			found = true;
-			break;
-		}
-	}
-
-	freeaddrinfo(res);
-	return found ? 0 : -EINVAL;
 }
 
 __cold

src/gwproxy/net.h
@@ -1,0 +1,34 @@
+/*
+ * utility for network-related stuff
+ */
+
+#include <arpa/inet.h>
+
+#define FULL_ADDRSTRLEN (INET6_ADDRSTRLEN + sizeof(":65535[]") - 1)
+
+struct gwp_sockaddr {
+	union {
+		struct sockaddr		sa;
+		struct sockaddr_in	i4;
+		struct sockaddr_in6	i6;
+	};
+};
+
+/*
+ * Convert address string to network address
+ *
+ * @param str source
+ * @param gs destination
+ * @return zero on success and a negative integer on failure.
+ */
+int convert_str_to_ssaddr(const char *str, struct gwp_sockaddr *gs);
+
+/*
+ * Convert network address to string format
+ *
+ * @param buf destination
+ * @param gs source
+ * @return zero on success and a negative integer on failure.
+ */
+int convert_ssaddr_to_str(char buf[FULL_ADDRSTRLEN],
+			const struct gwp_sockaddr *gs);

commit cbc022a61cefa5bf8acd249d785a065980d620f3

net: Guard _GNU_SOURCE definition
To fix redefinition warning.

Fixes: 70b88aa9a054 ("dnslookup: Split common functionality and struct into net.h and net.c")
Signed-off-by: Ammar Faizi <ammarfaizi2@gnuweeb.org>

src/gwproxy/net.c
@@ -1,4 +1,7 @@
+#ifndef _GNU_SOURCE
 #define _GNU_SOURCE
+#endif
+
 #include <stdio.h>
 #include <stddef.h>
 #include <errno.h>