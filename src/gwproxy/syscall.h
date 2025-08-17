realyukii/gwproxy

commit 5e7aa01e2958f46bbf1c035295370b8d98b062c3

log: Fix missing `gettid()` syscall on older glibc version
Compiling on GNU CC version 8.5.0 20210514 (Red Hat 8.5.0-26) with
glibc version 2.28 throws this error:

  src/gwproxy/log.c: In function ‘__pr_log’:
  src/gwproxy/log.c:51:56: warning: implicit declaration of function ‘gettid’; \
                                    did you mean ‘getgid’? [-Wimplicit-function-declaration]
    fprintf(handle, "[%s][%s][%08d]: %s\n", time_buf, ls, gettid(), pb);
                                                          ^~~~~~
                                                          getgid
  src/gwproxy/log.c:51: undefined reference to `gettid'
  collect2: error: ld returned 1 exit status

Fix it by defining our own `__sys_gettid()`.

Signed-off-by: Alviro Iskandar Setiawan <alviro.iskandar@gnuweeb.org>
Link: https://lore.gnuweeb.org/gwml/20250731222543.303821-2-alviro.iskandar@gnuweeb.org
Signed-off-by: Ammar Faizi <ammarfaizi2@gnuweeb.org>

src/gwproxy/syscall.h
@@ -283,6 +283,10 @@ static inline int __sys_eventfd(unsigned int c, int flags)
 	return (int) __do_syscall2(__NR_eventfd2, c, flags);
 }
 
+static inline pid_t __sys_gettid(void)
+{
+	return (pid_t)__do_syscall0(__NR_gettid);
+}
 #else /* #ifdef __x86_64__ */
 
 #include <errno.h>
@@ -429,6 +433,11 @@ static inline int __sys_shutdown(int sockfd, int how)
 {
 	int r = shutdown(sockfd, how);
 	return (r < 0) ? -errno : r;
+}
+
+static inline pid_t __sys_gettid(void)
+{
+	return syscall(__NR_gettid);
 }
 #endif /* #endif __x86_64__ */
 

src/gwproxy/log.c
@@ -1,5 +1,6 @@
 #include <gwproxy/common.h>
 #include <gwproxy/log.h>
+#include <gwproxy/syscall.h>
 #include <time.h>
 #include <stdlib.h>
 #include <stdio.h>
@@ -48,7 +49,7 @@ void __pr_log(FILE *handle, int level, const char *fmt, ...)
 	else
 		time_buf[0] = '\0';
 
-	fprintf(handle, "[%s][%s][%08d]: %s\n", time_buf, ls, gettid(), pb);
+	fprintf(handle, "[%s][%s][%08d]: %s\n", time_buf, ls, __sys_gettid(), pb);
 	if (unlikely(pb != loc_buf))
 		free(pb);
 out:

commit 06f772e2820e213db14fac91344586a1e272c5ce

syscall: Fix -Wshorten-64-to-32 warning on Android
Compiling gwproxy on Android results in these warnings:

  In file included from src/gwproxy/ev/epoll.c:8:
  In file included from ./src/gwproxy/ev/epoll.h:8:
  In file included from ./src/gwproxy/gwproxy.h:12:
  ./src/gwproxy/syscall.h:440:9: warning: implicit conversion loses integer precision: 'long' to 'pid_t' (aka 'int') [-Wshorten-64-to-32]
    440 |         return syscall(__NR_gettid);
        |         ~~~~~~ ^~~~~~~~~~~~~~~~~~~~
  In file included from src/gwproxy/gwproxy.c:7:
  In file included from ./src/gwproxy/gwproxy.h:12:
  ./src/gwproxy/syscall.h:440:9: warning: implicit conversion loses integer precision: 'long' to 'pid_t' (aka 'int') [-Wshorten-64-to-32]
    440 |         return syscall(__NR_gettid);
        |         ~~~~~~ ^~~~~~~~~~~~~~~~~~~~
  In file included from src/gwproxy/log.c:3:
  ./src/gwproxy/syscall.h:440:9: warning: implicit conversion loses integer precision: 'long' to 'pid_t' (aka 'int') [-Wshorten-64-to-32]
    440 |         return syscall(__NR_gettid);
        |         ~~~~~~ ^~~~~~~~~~~~~~~~~~~~

Fix this by casting the retval to pid_t.

Fixes: 5e7aa01e2958 ("log: Fix missing `gettid()` syscall on older glibc version")
Signed-off-by: Alviro Iskandar Setiawan <alviro.iskandar@gnuweeb.org>

src/gwproxy/syscall.h
@@ -437,7 +437,7 @@ static inline int __sys_shutdown(int sockfd, int how)
 
 static inline pid_t __sys_gettid(void)
 {
-	return syscall(__NR_gettid);
+	return (pid_t)syscall(__NR_gettid);
 }
 #endif /* #endif __x86_64__ */
 